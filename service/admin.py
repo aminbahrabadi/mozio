8960f83289e3445fc9103cf7b677cdb7210fa71a
Amin Bahrabadi <amin.bfamily@gmail.com>
2022-06-03 23:26:37 +0430

    Service: Add provider to admin

--- service/admin.py
+++ service/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.gis import admin
-from .models import ServiceArea
+from .models import ServiceArea, Provider
 
+admin.site.register(Provider)
 admin.site.register(ServiceArea, admin.GeoModelAdmin)
